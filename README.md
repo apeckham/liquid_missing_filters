3324d5194377e845362fc8149bf547aaf610ee88
apeckham <apeckham@gmail.com>
2016-01-22 23:14:49 -0800

    related links

---
+++ README.md
@@ -0,0 +1,5 @@
+Related links:
+
+* https://github.com/jekyll/jekyll/issues/3008
+* https://github.com/Shopify/liquid/issues/490
+* https://github.com/bluerail/liquid/commit/a7796ff431e5b3b7b8107251d59335a6a0154f99
--- liquid_test.rb
+++ liquid_test.rb
@@ -1,7 +1,3 @@
-# https://github.com/jekyll/jekyll/issues/3008
-# https://github.com/Shopify/liquid/issues/490
-# https://github.com/bluerail/liquid/commit/a7796ff431e5b3b7b8107251d59335a6a0154f99
-
 require 'liquid'
 require 'minitest/autorun'
 
