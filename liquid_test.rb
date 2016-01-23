ff28ae2ebf6098a9ed3986d4a103907c1c825243
apeckham <apeckham@gmail.com>
2016-01-22 22:57:57 -0800

    move tests around file system

--- liquid_test.rb
+++ liquid_test.rb
@@ -1,41 +1,42 @@
 # https://github.com/jekyll/jekyll/issues/3008
 # https://github.com/Shopify/liquid/issues/490
+# https://github.com/bluerail/liquid/commit/a7796ff431e5b3b7b8107251d59335a6a0154f99
 
 require 'liquid'
 require 'minitest/autorun'
 
 module Liquid
-  class Strainer
-    def invoke(method, *args)
-      if invokable?(method)
-        send(method, *args)
-      else
-        raise "Filter missing: #{method}" unless invokable?(method)
-      end
-    rescue ::ArgumentError => e
-      raise Liquid::ArgumentError.new(e.message)
-    end
-  end
+  # Monkey patches go here
 end
 
-# https://github.com/bluerail/liquid/commit/a7796ff431e5b3b7b8107251d59335a6a0154f99
-module Liquid
-  class Context
-    def lookup_and_evaluate(obj, key)
-      if (value = obj[key]).is_a?(Proc) && obj.respond_to?(:[]=)
-        obj[key] = (value.arity == 0) ? value.call : value.call(self)
-      elsif !obj.has_key?(key)
-        raise RuntimeError, "The variable `#{key}' is not defined"
-      else
-        value
+describe Liquid do
+  describe "file system" do
+    class InMemoryFileSystem
+      def read_template_file(file)
+        raise Errno::ENOENT if file == "error"
+        "Contents of #{file}"
       end
     end
-  end
-end
 
-describe Liquid::Template do
-  before do
-    Liquid::Template.file_system = Liquid::BlankFileSystem.new
+    after do
+      Liquid::Template.file_system = Liquid::BlankFileSystem.new
+    end
+
+    it 'does not include from BlankFileSystem' do
+      block = -> { Liquid::Template.parse("{% include 'open-graph-tags' %}").render! }
+      error = block.must_raise Liquid::FileSystemError
+      error.message.must_equal "Liquid error: This liquid context does not allow includes."
+    end
+
+    it 'includes from InMemoryFileSystem' do
+      Liquid::Template.file_system = InMemoryFileSystem.new
+      Liquid::Template.parse("{% include 'open-graph-tags' %}").render!.must_equal "Contents of open-graph-tags"
+    end
+
+    it 'includes a missing file from InMemoryFileSystem' do
+      Liquid::Template.file_system = InMemoryFileSystem.new
+      -> { Liquid::Template.parse("{% include 'error' %}").render! }.must_raise Errno::ENOENT
+    end
   end
 
   it 'renders' do
@@ -52,23 +53,6 @@ describe Liquid::Template do
 
   it 'renders a filter' do
     Liquid::Template.parse('{{ "sales" | append: ".jpg" }}').render!.must_equal 'sales.jpg'
-  end
-
-  it 'includes a file' do
-    block = -> { Liquid::Template.parse("{% include 'open-graph-tags' %}").render! }
-    error = block.must_raise Liquid::FileSystemError
-    error.message.must_equal "Liquid error: This liquid context does not allow includes."
-  end
-
-  it 'includes a file' do
-    class FakeFileSystem
-      def read_template_file(file)
-        "Content of #{file}"
-      end
-    end
-
-    Liquid::Template.file_system = FakeFileSystem.new
-    Liquid::Template.parse("{% include 'open-graph-tags' %}").render!.must_equal "Content of open-graph-tags"
   end
 
   it 'raises on syntax errors' do
