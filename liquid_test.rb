f4b481c40d20281083684fdd59269bf1c9995997
apeckham <apeckham@gmail.com>
2016-01-22 23:09:13 -0800

    separate into missing and new functionality

--- liquid_test.rb
+++ liquid_test.rb
@@ -10,69 +10,82 @@ module Liquid
 end
 
 describe Liquid do
-  describe "file system" do
-    after do
-      Liquid::Template.file_system = Liquid::BlankFileSystem.new
+  before { Liquid::Template.file_system = Liquid::BlankFileSystem.new }
+
+  describe "existing functionality" do
+    describe "file system" do
+      it "does not include from BlankFileSystem" do
+        block = -> { Liquid::Template.parse("{% include 'my-file' %}").render! }
+        error = block.must_raise Liquid::FileSystemError
+        error.message.must_equal "Liquid error: This liquid context does not allow includes."
+      end
+
+      it "includes a file" do
+        Liquid::Template.file_system = Class.new do
+          def self.read_template_file(file)
+            "Contents of #{file}"
+          end
+        end
+
+        Liquid::Template.parse("{% include 'my-file' %}").render!.must_equal "Contents of my-file"
+      end
+
+      it "includes a missing file" do
+        Liquid::Template.file_system = Class.new do
+          def self.read_template_file(_)
+            raise Errno::ENOENT
+          end
+        end
+
+        -> { Liquid::Template.parse("{% include 'error' %}").render! }.must_raise Errno::ENOENT
+      end
     end
 
-    it 'does not include from BlankFileSystem' do
-      block = -> { Liquid::Template.parse("{% include 'open-graph-tags' %}").render! }
-      error = block.must_raise Liquid::FileSystemError
-      error.message.must_equal "Liquid error: This liquid context does not allow includes."
-    end
+    describe "rendering" do
+      it "renders x" do
+        Liquid::Template.parse('hello {{ x }} world!').render!('x' => 5).must_equal 'hello 5 world!'
+      end
 
-    it 'includes a file' do
+      it "renders x.y" do
+        Liquid::Template.parse('{{ x.y }} {{ x.y }}').render!('x' => {'y' => 'foo'}).must_equal 'foo foo'
+      end
+
+      it "renders upcase filter" do
+        Liquid::Template.parse('{{ "foo" | upcase }}').render!.must_equal 'FOO'
+      end
+
+      it "renders append filter" do
+        Liquid::Template.parse('{{ "sales" | append: ".jpg" }}').render!.must_equal 'sales.jpg'
+      end
+
+      it "raises on syntax errors" do
+        block = -> { Liquid::Template.parse('{% if %}') }
+        error = block.must_raise Liquid::SyntaxError
+        error.message.must_equal "Liquid syntax error: Syntax Error in tag 'if' - Valid syntax: if [expression]"
+      end
+    end
+  end
+
+  describe "new functionality" do
+    it "saves a list of missing includes" do
       Liquid::Template.file_system = Class.new do
         def self.read_template_file(file)
-          "Contents of #{file}"
+          raise Errno::ENOENT unless file == "existing"
         end
       end
 
-      Liquid::Template.parse("{% include 'open-graph-tags' %}").render!.must_equal "Contents of open-graph-tags"
+      Liquid::Template.parse("{% include 'missing' %} {% include 'existing' %}").render!
+      Liquid::Template.missing_includes.must_equal ["missing"]
     end
 
-    it 'includes a missing file' do
-      Liquid::Template.file_system = Class.new do
-        def self.read_template_file(_)
-          raise Errno::ENOENT
-        end
-      end
-
-      -> { Liquid::Template.parse("{% include 'error' %}").render! }.must_raise Errno::ENOENT
+    it "saves a list of missing variables" do
+      Liquid::Template.parse(".. {{ x }} {{ x.y }} !!").render!({'x' => 5}).must_equal ".. 5  !!"
+      Liquid::Template.missing_variables.must_equal ["x.y"]
     end
-  end
 
-  it 'renders' do
-    Liquid::Template.parse('{{ x }}').render!('x' => 5).must_equal '5'
-  end
-
-  it 'renders' do
-    Liquid::Template.parse('{{ x.y }}').render!('x' => {'y' => 6}).must_equal '6'
-  end
-
-  it 'renders a filter' do
-    Liquid::Template.parse('{{ "x" | upcase }}').render!.must_equal 'X'
-  end
-
-  it 'renders a filter' do
-    Liquid::Template.parse('{{ "sales" | append: ".jpg" }}').render!.must_equal 'sales.jpg'
-  end
-
-  it 'raises on syntax errors' do
-    block = -> { Liquid::Template.parse('{% if %}') }
-    error = block.must_raise Liquid::SyntaxError
-    error.message.must_equal "Liquid syntax error: Syntax Error in tag 'if' - Valid syntax: if [expression]"
-  end
-
-  it 'raises when a variable is missing' do
-    block = -> { Liquid::Template.parse("{{ x }}").render!({}) }
-    error = block.must_raise RuntimeError
-    error.message.must_equal "The variable `x' is not defined"
-  end
-
-  it 'raises when a filter is missing' do
-    block = -> { Liquid::Template.parse("{{ 1 | x }}").render! }
-    error = block.must_raise RuntimeError
-    error.message.must_equal "Filter missing: x"
+    it "saves a list of missing filters" do
+      Liquid::Template.parse("{{ 'foobar' | upcase | camelcase }}").render!({}).must_equal "FOOBAR"
+      Liquid::Template.missing_filters.must_equal ["camelcase"]
+    end
   end
 end
