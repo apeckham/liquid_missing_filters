59ebb3cd70befbfba66e4515dfc77557a7afcb68
Ivan Kuznetsov <me@jeiwan.ru>
2016-01-26 20:17:57 +0700

    Save a list of used and missing filters

--- liquid_ext.rb
+++ liquid_ext.rb
@@ -1,19 +1,23 @@
 module Liquid
   class Template
-    def render_with_info(*args)
+    def render_with_info!(*args)
+      render_with_info(*args)
     end
 
-    def render_with_info!(*args)
-      result = render!(*args)
+    def render_with_info(*args)
+      raise ArgumentError unless args.first.is_a? Hash
+      context = Context.new([args.shift, assigns], instance_assigns, registers, @rethrow_errors, @resource_limits)
+
+      result = render(context, args.last)
+
       [
         result,
         {
-          all_variables: _all_variables,
           included_files: [],
-          missing_filters: [],
-          missing_variables: _missing_variables(*args),
-          used_filters: [],
-          used_variables: _used_variables
+          missing_filters: _missing_filters(context),
+          missing_variables: _missing_variables(context),
+          used_filters: _used_filters(context),
+          used_variables: _used_variables(context)
         }
       ]
     end
@@ -23,30 +27,45 @@ module Liquid
     def _all_variables
       @_all_variables ||= @root.nodelist.map do |node|
         if node.is_a?(Liquid::Variable)
-          variable_name = node.name.name
-          variable_name << ".#{node.name.lookups.join('.')}" if node.name.lookups.any?
-          variable_name
+          if node.name.is_a?(Liquid::VariableLookup)
+            variable_name = node.name.name
+            variable_name << ".#{node.name.lookups.join('.')}" if node.name.lookups.any?
+            variable_name
+          else
+            nil
+          end
         else
           nil
         end
       end.compact
     end
 
-    def _used_variables
-      _all_variables - _missing_variables
+    def _all_filters
+      @_all_filters ||= @root.nodelist.flat_map do |node|
+        if node.is_a?(Liquid::Variable)
+          node.filters.map { |filter| filter[0] }
+        else
+          nil
+        end
+      end.compact
     end
 
-    def _missing_variables(*args)
-      @_missing_variables ||= begin
-        registers = case args.first
-        when Liquid::Context
-          args.first.registers
-        when Hash
-          args.first.keys
-        end
+    def _used_variables(context)
+      _all_variables - _missing_variables(context)
+    end
 
-        _all_variables - registers
+    def _missing_variables(context)
+      @_missing_variables ||= _all_variables - context.environments.flat_map(&:keys)
+    end
+
+    def _used_filters(context)
+      @_used_filters ||= _all_filters.select do |filter|
+        context.strainer.class.invokable? filter
       end
+    end
+
+    def _missing_filters(context)
+      _all_filters - _used_filters(context)
     end
   end
 end
